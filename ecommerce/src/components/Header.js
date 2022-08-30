b41120b8b715e03df87db3836287b71286ec7e11
Zain Khan <m.zainkhan1996@gmail.com>
2022-08-30 20:35:20 +0500

    Updated HeadJS title to clothix

--- ecommerce/src/components/Header.js
+++ ecommerce/src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='ui fixed menu'>
         <div className="ui container center">
-            <h2>Clothing Brand</h2>
+            <h2>Clothix</h2>
         </div>
     </div>
   )
